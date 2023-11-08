ad7161cbe1bbded70bea569716ce8c3935f5265a
priyans-hu <gargpriyanshu2004@gmail.com>
2023-11-09 00:27:20 +0530

    changes

--- src/components/FormAction.js
+++ src/components/FormAction.js
@@ -1,11 +1,6 @@
-import React , { useState } from 'react'
+import React from 'react'
 
-export default function FormAction(props) {
-    const [display,setDisplay] = useState("true");    
-    
-    function deleteButtonHandler(){
-        console.log('Delete button handler');
-    }
+export default function FormAction(props) {    
     function editButtonHandler(){
         console.log('Edit button handler');
     }
@@ -14,7 +9,7 @@ export default function FormAction(props) {
             <button type="button" title='Edit Task' className="btn btn-primary mx-2" onClick={editButtonHandler}>
                 <i className="fa-solid fa-pen"></i>
             </button>
-            <button type="button" title='Delete Task' className="btn btn-danger" onClick={deleteButtonHandler}>
+            <button type="button" title='Delete Task' className="btn btn-danger" onClick={props.deleteButtonHandler}>
                 <i className="fa-regular fa-trash-can"></i>
             </button>
         </span>
--- src/components/PendingTaskListItem.js
+++ src/components/PendingTaskListItem.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CheckBox from './CheckBox.js';
 import FormActions from './FormAction.js';
+import ReactDOM from "react-dom/client";
 
 export default function PendingTaskListItem(props) {
+    const [show, setShow] = useState(true);
+    function deleteButtonHandler(){
+        console.log("deleteButtonHandler");
+        setShow(!show);
+    }
+
     return (
-        <li className="list-group-item bg-secondary col-lg-5 text-left d-flex justify-content-between align-items-center my-2 rounded">
+        <li className="list-group-item bg-secondary col-lg-5 text-left d-flex justify-content-between align-items-center my-2 rounded" style={{display:show?"flex":"none"}}>
             <div>
                 <CheckBox />
                 <p className=' m-0 pl-4 d-inline'>{props.taskname}</p>
             </div>
-            <FormActions />
+            <FormActions deleteButtonHandler={deleteButtonHandler}/>
         </li>
     );
 };
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<PendingTaskListItem />)
